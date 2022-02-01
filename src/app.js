409c24ee660387b8797074f6ccfbb107db04e160
blomsadze <85993476+Naka1910@users.noreply.github.com>
2022-02-01 18:54:08 +0400

    add some changes

--- src/app.js
+++ src/app.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import React from 'react-dom';
+// let arr = ['JavasCript', 'Phyton', 'C#', 'C++', 'C', 'Swift', 'Kotlin'];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i].length > 5) {
+//     continue;
+
+//   }
+//   console.log(arr[i])
+// }
+// let nums = [[2, -3, 5, 11], [1, -30, -11, 103], [- 1, -2, -4]];
+// for (let i = 0; i < nums.length; i++) {
+//   const numbers = nums[i];
+//   for (let j = 0; j < numbers.length; j++) {
+//     const n = numbers[j]
+//     if (n > 0) {
+//       console.log(numbers[j])
+//     }
+
+
+//   }
+// }
 
 
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Maltesse",
-    }),
-    React.createElement(Pet, { name: "Sesi", animal: "Cat", breed: "Brcat" }),
-    React.createElement(Pet, { name: "Londa", animal: "Dog", breed: "york" }),
-  ]);
 
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+// function getMax(numbs) {
+//   let max = -Infinity;
+//   for (let i = 0; i < numbs.length; i++) {
+//     let x = numbs[i];
+//     if (x > max) {
+//       max = x;
+//     }
+//   }
+//   return max;
+// }
+// console.log(getMax(numbs));
+
+// let minNum = Math.min(...numbs);
+// console.log(minNum);
+// let getMax = [1, 9, 3, 58];
+// getMax.sort((a, b) => (a + b));
+// console.log(getMax[0])
+
+
+// function printName(user = {
+//   firstname: 'Naka',
+//   lastname: 'Jikia',
+//   age: 25,
+//   isLogged: true,
+// }) {
+//   if (!user.isLogged) {
+//     return user.firstname + ' ' + user.lastname
+//   } return false;
+// }
+
+
+// console.log(printName());
+
+let arr = [20, 14, 18, false, null, 35, 40];
+for (let i = 0; i < arr.length; i++) {
+
+  if (typeof arr[i] === 'number' && arr[i] % 5 === 0) {
+    console.log(arr[i]);
+  }
+}
+
+
+let numbs = [8, 6, 66, 45, -2];
+
+let x = 0;
+while (x < numbs.length) {
+  console.log(numbs[x])
+  x++;
+}  
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+
+// function printName(user = {
+//   firstname: 'Naka',
+//   lastname: 'Jikia',
+//   age: 25,
+//   isLogged: true,
+// }) {
+//   if (!user.isLogged) {
+//     return user.firstname + ' ' + user.lastname
+//   } return false;
+// }
